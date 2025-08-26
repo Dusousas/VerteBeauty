b3023989d908d4da30af993fcf96b3763dc59911
dusousas <dusousadeveloper@gmail.com>
2025-08-26 15:41:21 -0300

    section tuturial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CardsMain from "../../components/CardsMain";
 import Main from "../../components/Main";
+import Tutorial from "../../components/Tutorial";
 
 export default function Home() {
   return (
     <>
       <Main />
       <CardsMain />
+      <Tutorial />
     </>
   );
 }
